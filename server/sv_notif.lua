f48b64837d9ea4d402165561515a2e7fd1a91b9a
KC-1508 <107610683+KC-1508@users.noreply.github.com>
2022-07-05 19:43:13 -0500

    refactor: \server\ edits

--- server/sv_notif.lua
+++ server/sv_notif.lua
@@ -2,9 +2,24 @@ local notif = true --[[
     Enabled by default, "true"...
 ]]
 
+local rel = {
+    "1.0.0-PROOF", --[[
+        Ship 7/3/22,
+        Expire 7/17/22..
+    ]]
+    "1.0.0-PROOF-2", --[[
+        Ship 7/4/22,
+        Expire 7/18/22..
+    ]]
+    "1.0.0-PROOF-3" --[[
+        Ship 7/5/22,
+        Expire 7/19/22..
+    ]]
+}
+
 CreateThread(function()
     if notif then
         Wait(750)
-        print("\n   🍊🍊🍊🍊🍊🍊🍊\n   Tangerines, anyone?\n")
+        print("\n" .. "Tangerines, anyone?" .. "\n" .. "🍊 On release " .. rel[3] .. "!" .. "\n")
     end
 end)
